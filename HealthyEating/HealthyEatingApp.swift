4970a53aa21b6411b18558486f7e7fab735dc646
Prabhu <prabhu.arokiasamy8@gmail.com>
2024-06-11 21:42:09 -0700

    Initial Commit

---
+++ HealthyEating/HealthyEatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyEatingApp.swift
+//  HealthyEating
+//
+//  Created by TomHarry on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthyEatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
